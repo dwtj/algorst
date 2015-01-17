e89f92c2a67e371721e3a126f9ec31702e4a5277
David Johnston <dwtj@dwtj.me>
2015-01-16 23:53:30 -0600

    Moved `boxes` module into its own file.

---
+++ src/boxes.rs
@@ -0,0 +1,13 @@
+pub struct MyBox<T: Clone> {
+    data: T,
+}
+
+impl<T: Clone> MyBox<T> {
+    pub fn new(d: T) -> Self {
+        MyBox { data: d }
+    }
+
+    pub fn get(&self) -> T {
+        self.data.clone()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,1 @@
-pub mod boxes {
-    pub struct MyBox<T: Clone> {
-        data: T,
-    }
-
-    impl<T: Clone> MyBox<T> {
-        pub fn new(d: T) -> Self {
-            MyBox { data: d }
-        }
-
-        pub fn get(&self) -> T {
-            self.data.clone()
-        }
-    }
-}
+pub mod boxes;
