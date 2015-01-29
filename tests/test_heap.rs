a9b1727725250341408213017f57c5e2789dfe58
David Johnston <dwtj@iastate.edu>
2015-01-29 01:56:26 -0600

    Fixed bug in Heap test.
    Also moved heap logging of a `.dot` file into its own (ugly) function.

--- tests/test_heap.rs
+++ tests/test_heap.rs
@@ -14,6 +14,15 @@ fn make_heap() -> Heap<u32> {
     heap
 }
 
+fn log_heap(heap: &Heap<u32>, p: &Path) {
+    let mut dot_file = match File::create(p) {
+        Ok(f) => f,
+        Err(e) => panic!("file error: {}", e),
+    };
+
+    graphviz::render(heap, &mut dot_file);
+}
+
 #[test]
 fn test_single_push() {
     let mut heap: Heap<u32> = Heap::new();
@@ -42,23 +51,29 @@ fn test_more_push_and_pop() {
     }
 
     let expected = vec!(541, 531, 511, 352, 342, 342, 42, 0);
+    let mut actual = Vec::new();
     for idx in (0..expected.len()) {
-        let popped = heap.pop();
-        assert_eq!(heap.pop().unwrap(), expected[idx]);
+        actual.push(heap.pop().unwrap());
     }
 
-    assert!(heap.empty());
+    let p = Path::new("tests/logs/heap_push_and_pop.log");
+    let mut f = File::create(&p).unwrap();
+    f.write_fmt(format_args!("expected: {:?}\n", expected));
+    f.write_fmt(format_args!("actual:   {:?}\n", actual));
+    assert_eq!(expected, actual);
+    assert_eq!(heap.len(), 0);
+}
+
+#[test]
+fn test_graphviz_render_empty() {
+    let heap = Heap::new();
+    let p = Path::new("tests/logs/empty_heap.dot");
+    log_heap(&heap, &p);
 }
 
 #[test]
 fn test_graphviz_render() {
     let heap = make_heap();
-
     let p = Path::new("tests/logs/heap.dot");
-    let mut dot_file = match File::create(&p) {
-        Ok(f) => f,
-        Err(e) => panic!("file error: {}", e),
-    };
-
-    graphviz::render(&heap, &mut dot_file);
+    log_heap(&heap, &p);
 }
